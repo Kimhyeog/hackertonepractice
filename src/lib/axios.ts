af9a60a3c45a0a099e11bd94edb8e5db61cf7286
19013219_kim <cyworld5057@naver.com>
2025-06-14 22:36:43 +0900

    - 로그인, 유저 정보에 대한 API 함수 작성 세팅 코드

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,9 +1,15 @@
 import axios from "axios";
 
+//인증이 필요한 API의 axios 인스턴스
 export const API = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
-  withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-  },
+  withCredentials: true, // 로그인 필요 API용
+  headers: { "Content-Type": "application/json" },
+});
+
+//인증이 필요없는 API의 axios 인스턴스
+export const PublicAPI = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  withCredentials: false, // 인증 없이 사용하는 API용
+  headers: { "Content-Type": "application/json" },
 });
